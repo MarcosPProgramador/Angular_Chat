9fa9fb13431c75813804ac0521dfbda7a86e5cdc
MarcosPProgramador <64386374+MarcosPProgramador@users.noreply.github.com>
2020-09-07 19:57:13 -0300

    Added chat

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -14,38 +14,45 @@ export class ListComponent implements OnInit {
                 name: 'Marcos Proença',
                 message: 'Bom diaa!',
                 date: '07/09/2020',
-                hours : '10:12',
-                image: './img/user-joao.jpg',
+                hours: '10:12',
                 vip: true,
+                show: true,
             },
             {
                 id: 1,
                 name: 'João Gonalve',
                 message: 'E aí seu otário',
                 date: '07/09/2020',
-                hours : '10:16',
-                image: './img/user-joao.jpg',
+                hours: '10:16',
                 vip: false,
+                show: false,
             },
             {
                 id: 2,
                 name: 'Joana Brina',
                 message: 'Pra que isso, seu ignorante?',
                 date: '07/09/2020',
-                hours : '10:17',
-                image: './img/user-joao.jpg',
+                hours: '10:17',
                 vip: false,
+                show: true,
             },
             {
                 id: 3,
                 name: 'Laura Souza',
                 message: 'Isto é completamente normal aqui Joana',
                 date: '07/09/2020',
-                hours : '11:29',
-                image: './img/user-joao.jpg',
+                hours: '11:29',
                 vip: true,
+                show: true,
             },
         ];
+
+    }
+    userClick(e) {
+        e.currentTarget.classList.toggle('active');
+    }
+    deleteComment(){
+        
     }
 
     ngOnInit(): void {}
--- src/app/models/app.list.ts
+++ src/app/models/app.list.ts
@@ -1,9 +1,9 @@
 export class List {
-  id: number;
-  name: string;
-  message: string;
-  date: string;
-  hours : string;
-  image: string;
-  vip: boolean;
+    id: number;
+    name: string;
+    message: string;
+    date: string;
+    hours: string;
+    vip: boolean;
+    show: boolean;
 }
